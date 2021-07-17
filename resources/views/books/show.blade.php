d58e9c29a5b84cb60e23033c8550524ddf360b66
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-17 14:23:11 +0900

    [Add]components

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Bookers | Laravel</title>
-    <link rel="stylesheet" href="{{ url('css/style.css') }}">
-</head>
-<body>
-    <div class="container">
-        <div class="back-link">
-            &laquo; <a href="{{ route('books.index') }}">Back</a>
-        </div>
-        <h1>{{ $book }}</h1>
+<x-layout>
+    <div class="back-link">
+        &laquo; <a href="{{ route('books.index') }}">Back</a>
     </div>
-</body>
-</html>
+    <h1>{{ $book }}</h1>
+</x-layout>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Bookers | Laravel</title>
-    <link rel="stylesheet" href="{{ url('css/style.css') }}">
-</head>
-<body>
-    <div class="container">
-        <h1>Laravel Bookers</h1>
+<x-layout>
+    <h1>Laravel Bookers</h1>
         <p>Laravelを使ったBookersです</p>
         <ul>
             @forelse ($books as $index => $book)
@@ -20,6 +12,4 @@
                 <li>本がありません。</li>
             @endforelse
         </ul>
-    </div>
-</body>
-</html>
+</x-layout>
