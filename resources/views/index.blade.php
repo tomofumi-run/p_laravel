2a2126086d1723759a654cc1cb33247c9a1f27a2
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-17 13:23:43 +0900

    [Add]check route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Bookers | Laravel</title>
+</head>
+<body>
+    <h1>Laravel Bookers</h1>
+    <p>Laravelを使ったBookersです</p>
+    <ul>
+        <li>Book1</li>
+        <li>Book2</li>
+        <li>Book3</li>
+    </ul>
+</body>
+</html>
