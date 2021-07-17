741ae3a6814c57ce877d01de230cf7957761538d
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-17 13:27:12 +0900

    [Add]layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Bookers | Laravel</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Laravel Bookers</h1>
-    <p>Laravelを使ったBookersです</p>
-    <ul>
-        <li>Book1</li>
-        <li>Book2</li>
-        <li>Book3</li>
-    </ul>
+    <div class="container">
+        <h1>Laravel Bookers</h1>
+        <p>Laravelを使ったBookersです</p>
+        <ul>
+            <li>Book1</li>
+            <li>Book2</li>
+            <li>Book3</li>
+        </ul>
+    </div>
 </body>
 </html>
