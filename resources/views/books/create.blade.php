f63e7f7db5f258e2247f972cc733dfb3e51ef776
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-18 21:41:54 +0900

    [Add]Create Form

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -6,4 +6,17 @@
         &laquo; <a href="{{ route('books.index') }}">Back</a>
     </div>
     <h1>新規投稿ページ</h1>
+
+    <form action="" method="post">
+        @csrf {{-- トークン作成--}}
+        <label>
+            Title
+            <input type="text" name="title">
+        </label>
+        <label>
+            Body
+            <textarea name="body"></textarea>
+        </label>
+        <button>投稿する</button>
+    </form>
 </x-layout>
